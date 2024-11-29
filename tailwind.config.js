623d5e816c0b365f5f96bf2d9f6ff4db613a9f6a
yejineee <yygeniee@gmail.com>
2024-11-29 18:12:22 +0900

    fix: 폰트 적용 방법 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				thin: ["Pretendard-Thin", ...defaultTheme.fontFamily.sans],
-				extralight: ["Pretendard-ExtraLight", ...defaultTheme.fontFamily.sans],
-				light: ["Pretendard-Light", ...defaultTheme.fontFamily.sans],
-				normal: ["Pretendard-Regular", ...defaultTheme.fontFamily.sans],
-				medium: ["Pretendard-Medium", ...defaultTheme.fontFamily.sans],
-				semibold: ["Pretendard-SemiBold", ...defaultTheme.fontFamily.sans],
-				bold: ["Pretendard-Bold", ...defaultTheme.fontFamily.sans],
-				extrabold: ["Pretendard-ExtraBold", ...defaultTheme.fontFamily.sans],
-				black: ["Pretendard-Black", ...defaultTheme.fontFamily.sans],
-				sans: ["Pretendard-Regular", ...defaultTheme.fontFamily.sans],
-				bold: ["Pretendard-Bold", ...defaultTheme.fontFamily.sans],
+				sans: ["Pretendard", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
