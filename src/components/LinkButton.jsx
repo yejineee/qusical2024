2d51e810348152af0644387db33e1adb398e4d02
yejineee <yygeniee@gmail.com>
2024-11-30 00:27:25 +0900

    feat: Button에 아이콘 추가

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,4 +1,22 @@
-function LinkButton({ onClick, children }) {
+function LinkButton({ children, outerLink, icon }) {
+	const onClick = () => {
+		if (outerLink) {
+			return window.open(outerLink, "_blank", "noopener,noreferrer");
+		}
+	};
+
+	if (icon) {
+		return (
+			<button
+				className="flex justify-center items-center px-4 w-full h-12 bg-white rounded-3xl shadow-md"
+				onClick={onClick}
+			>
+				<div className="absolute left-8 w-6 h-6">{icon}</div>
+				<div>{children}</div>
+			</button>
+		);
+	}
+
 	return (
 		<button
 			className="w-full h-12 bg-white rounded-3xl shadow-md"
