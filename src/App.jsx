cd939dc04d1b887135b602902ea0f10b11c0cd8d
yejineee <yygeniee@gmail.com>
2024-11-30 20:48:01 +0900

    share 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import YoutubeBlock from "./components/YoutubeBlock";
 function App() {
 	return (
 		<div className="mr-auto ml-auto max-w-screen-sm">
+			<Share />
 			<div className="flex flex-col gap-8 justify-between px-4 py-16 w-full h-full">
 				<ProfileSection />
 				<TwoByTwoSection />
@@ -21,3 +22,28 @@ function App() {
 }
 
 export default App;
+
+const data = {
+	title: "2025 큐지컬 - 유다",
+	url: "https://qusical2024.vercel.app/",
+};
+
+function Share() {
+	const handleClick = async () => {
+		try {
+			await navigator.share(data);
+			alert("success");
+		} catch {
+			alert("error!");
+		}
+	};
+
+	if (!navigator.share) {
+		return;
+	}
+	return (
+		<div className="absolute top-4 right-4">
+			<button onClick={handleClick}>share</button>
+		</div>
+	);
+}
