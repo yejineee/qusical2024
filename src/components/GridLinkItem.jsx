8923b2af2f24b0814cb0d27cfb221dd66a2445b1
yejineee <yygeniee@gmail.com>
2024-11-30 23:52:37 +0900

    feat: 넘버 이미지 변경

--- src/components/GridLinkItem.jsx
+++ src/components/GridLinkItem.jsx
@@ -1,12 +1,16 @@
+import { twMerge } from "tailwind-merge";
 import BlockContainer from "./BlockContainer";
 
-function GridLinkItem({ subtitle, children }) {
+function GridLinkItem({ subtitle, children, src, imgClassName }) {
 	return (
 		<BlockContainer className="flex flex-col gap-4 p-2">
 			<div className="flex flex-col justify-center items-center p-4 w-full h-full text-sm rounded-2xl bg-brown">
 				<img
-					src="/image/img1.jpg"
-					className="object-cover w-16 h-16 rounded-full"
+					src={src ?? "/image/img1.jpg"}
+					className={twMerge(
+						"object-cover w-16 h-16 rounded-full",
+						imgClassName,
+					)}
 				></img>
 				<div className="pt-2 font-semibold text-white">
 					{subtitle ?? children}
--- src/components/TwoByTwoSection.jsx
+++ src/components/TwoByTwoSection.jsx
@@ -5,7 +5,9 @@ function TwoByTwoSection() {
 		<div className="grid grid-cols-2 gap-x-4 gap-y-4 grid-flow-dense">
 			<GridLinkItem>공연 순서</GridLinkItem>
 			<GridLinkItem>인물관계도</GridLinkItem>
-			<GridLinkItem>넘버</GridLinkItem>
+			<GridLinkItem src="/image/LP.webp" imgClassName="object-fill">
+				넘버
+			</GridLinkItem>
 			<GridLinkItem subtitle="극단대장부">큐지컬을 만든 사람들</GridLinkItem>
 		</div>
 	);
