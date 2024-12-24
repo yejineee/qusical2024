a3cc42d309b2e342db22b8fcef58599381d2d5ca
yejineee <yygeniee@gmail.com>
2024-12-25 03:26:42 +0900

    chore: Analytics vercel 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,11 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-
+import { Analytics } from "@vercel/analytics/react";
 export const Route = createRootRoute({
 	component: () => (
 		<div className="max-w-screen-sm px-4 py-12 ml-auto mr-auto">
 			<Outlet />
+			<Analytics />
 			{import.meta.env.DEV && <TanStackRouterDevtools />}
 		</div>
 	),
