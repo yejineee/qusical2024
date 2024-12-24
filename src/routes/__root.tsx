b5d46cca7d7575e7a45e2f50d9c70ebbd5295662
yejineee <yygeniee@gmail.com>
2024-12-25 03:30:07 +0900

    Revert "chore: 성능측정 추가"
    This reverts commit 2986e57ed1c3a93da438820c73e4a4d6ea6109af.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 export const Route = createRootRoute({
 	component: () => (
 		<div className="max-w-screen-sm px-4 py-12 ml-auto mr-auto">
