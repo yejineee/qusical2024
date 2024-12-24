9a771ef42a54ea8795f1ca5637266bf1c5fcee89
yejineee <yygeniee@gmail.com>
2024-12-25 03:32:44 +0900

    perf: 이미지 성능 최적화

--- src/routes/relation.tsx
+++ src/routes/relation.tsx
@@ -8,7 +8,11 @@ export const Route = createFileRoute("/relation")({
 function Relation() {
 	return (
 		<SubPageTemplate title="인물관계도">
-			<img src="/image/relation.png" alt="인물관계도" />
+			<img
+				fetchPriority="high"
+				src="https://t1.kakaocdn.net/thumb/@2x.fwebp.q75/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fone%2Fprofile%2F20241225030554-54335664-b76e-40b3-8363-d9935aad37f6.png"
+				alt="인물관계도"
+			/>
 		</SubPageTemplate>
 	);
 }
