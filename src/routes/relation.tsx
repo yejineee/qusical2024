2819974910b5591d8c2790f394cd6ee49cf83810
yejineee <yygeniee@gmail.com>
2024-12-25 02:53:54 +0900

    feat: 인물관계도 이미지 추가

--- src/routes/relation.tsx
+++ src/routes/relation.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SubPageTemplate from "~/components/SubPageTemplate";
+
 export const Route = createFileRoute("/relation")({
 	component: Relation,
 });
 
 function Relation() {
-	return <SubPageTemplate title="인물관계도">{""}</SubPageTemplate>;
+	return (
+		<SubPageTemplate title="인물관계도">
+			<img src="/image/relation.png" alt="인물관계도" />
+		</SubPageTemplate>
+	);
 }
