c95278823c6e468aa22b8c308ee163c0d4237e59
yejineee <yygeniee@gmail.com>
2024-12-25 23:22:30 +0900

    feat: 카카오톡 채널 링크 추가

--- src/components/LinkSection.jsx
+++ src/components/LinkSection.jsx
@@ -3,8 +3,11 @@ import { ICON_COLOR } from "./Icon";
 import IconLinkButton from "./IconLinkButton";
 
 function LinkSection() {
+	const onClickChannel = () => {
+		window.open("http://pf.kakao.com/_QuSBxj/friend", "_blank");
+	};
 	return (
-		<div className="flex gap-8 justify-center">
+		<div className="flex justify-center gap-8">
 			<IconLinkButton url="https://www.instagram.com/the.daejangbu/">
 				<SlSocialInstagram size="28" color={ICON_COLOR} />
 			</IconLinkButton>
@@ -14,6 +17,12 @@ function LinkSection() {
 			<IconLinkButton url="https://woori.cc/">
 				<SlHome size="28" color={ICON_COLOR} />
 			</IconLinkButton>
+			<img
+				onClick={onClickChannel}
+				width="32"
+				height="32"
+				src="https://t1.kakaocdn.net/one/production/front/_next/static/media/channel_type2.4e470592.svg"
+			/>
 		</div>
 	);
 }
