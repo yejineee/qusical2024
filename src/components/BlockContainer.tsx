2df2390c87e45fdc20089da3df6fa50304953e63
yejineee <yygeniee@gmail.com>
2024-12-25 22:49:25 +0900

    feat: 극단대장부 페이지 추가

---
+++ src/components/BlockContainer.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
+function BlockContainer({
+	className,
+	children,
+}: { className?: string; children: React.ReactNode }) {
+	return (
+		<div className={twMerge("p-4 bg-white rounded-3xl shadow-md", className)}>
+			{children}
+		</div>
+	);
+}
+
+export default BlockContainer;
--- src/routes/daejangbu.tsx
+++ src/routes/daejangbu.tsx
@@ -1,10 +1,107 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SubPageTemplate from "~/components/SubPageTemplate";
+import BlockContainer from "~/components/BlockContainer";
 
 export const Route = createFileRoute("/daejangbu")({
 	component: Daejangbu,
 });
 
+const Intro = () => (
+	<article>
+		<b className="font-hambak">“너는 힘써 대장부가 되고”</b>
+		&nbsp;(열왕기상 2:2)
+		<br />
+		<br />
+		극단대장부는 큐지컬(QT+Musical)을 제작하는 우리들교회의 문화사역부서입니다.
+		<br />
+		우리부터 말씀으로 살아나고, 그 은혜를 관객들에게 전하고자 합니다.
+	</article>
+);
+function HistoryYoutubeBottom({ url }: { url: string }) {
+	{
+		return (
+			<div className="flex items-center justify-end">
+				<a
+					className="px-4 py-3 font-semibold rounded-3xl border border-stone-800 min-w-[48px] active:bg-slate-400"
+					href={url}
+					target="_blank"
+				>
+					Youtube
+				</a>
+			</div>
+		);
+	}
+}
+
+function HistoryBlock({
+	title,
+	img,
+	children,
+	youtubeURL,
+}: {
+	title: string;
+	img?: string;
+	youtubeURL?: string;
+	children?: React.ReactNode;
+}) {
+	return (
+		<BlockContainer>
+			<div className="flex items-center">
+				<div className="font-semibold text-md">{title}</div>
+			</div>
+			<div className="my-4">
+				{img && <img src={img} alt={title} />}
+				{children}
+			</div>
+			{youtubeURL && <HistoryYoutubeBottom url={youtubeURL} />}
+		</BlockContainer>
+	);
+}
+
+const History = () => (
+	<div>
+		<h2 className="pb-4 text-xl font-bold">큐지컬 히스토리</h2>
+		<ul className="flex flex-col gap-4">
+			<HistoryBlock title="시즌1 ‘기업 무르기’ (2018년 1월)">
+				<iframe
+					width="100%"
+					height="100%"
+					src="https://www.youtube.com/embed/h-WADSaff3Q?si=tsZUOsLR9egA3jFb"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerpolicy="strict-origin-when-cross-origin"
+					allowfullscreen
+				></iframe>
+			</HistoryBlock>
+			<HistoryBlock
+				title="시즌2 ‘압살롬의 기념비’ (2019년 1월)"
+				img="/image/q2.jpg"
+				youtubeURL="https://youtu.be/UDU07D2B8lc?si=1cieXGa4VLmysE_7"
+			/>
+			<HistoryBlock title="시즌3 ‘갈멜’ (2020년 1월)" img="/image/q3.jpg" />
+			<HistoryBlock
+				title="시즌4 ‘증인’ (2022년 12월)"
+				img="/image/q4.jpeg"
+				youtubeURL="https://youtu.be/2lc8R_Xsa30?si=dGGQHtreS9Q0MYEE"
+			/>
+			<HistoryBlock
+				title="시즌5 ‘상속자’ (2024년 1월)"
+				img="/image/q5.jpg"
+				youtubeURL="https://youtu.be/N75TlmEqhqs?si=VVJDTbz17Ja5FpJa"
+			/>
+			<HistoryBlock title="시즌6 ‘증인’ (2025년 1월)" img="/image/q6.png" />
+		</ul>
+	</div>
+);
+
 function Daejangbu() {
-	return <SubPageTemplate title="극단대장부">{""}</SubPageTemplate>;
+	return (
+		<SubPageTemplate title="극단대장부">
+			<div className="flex flex-col gap-4 container-sub">
+				<Intro />
+				<History />
+			</div>
+		</SubPageTemplate>
+	);
 }
