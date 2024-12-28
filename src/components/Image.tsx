d030ebd6bc69bddde26d94843c7b70a456a8ecc2
yejineee <yygeniee@gmail.com>
2024-12-28 23:37:32 +0900

    chore: webp 옵션 추가

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -8,7 +8,7 @@ const DAUM_CDN = "//i1.daumcdn.net/thumb/";
 const IMAGE_URL = "https://qusical2025.vercel.app/image/";
 
 function Image({ file, quality = 70, size, ...props }: ImageProps) {
-	const option = encodeURIComponent(`C${size}x${size}`);
+	const option = encodeURIComponent(`C${size}x${size}.fwebp`);
 	const filePath = encodeURIComponent(`${IMAGE_URL}${file}`);
 	const src = `${DAUM_CDN}${option}/?fname=${filePath}`;
 	return <img src={src} {...props}></img>;
