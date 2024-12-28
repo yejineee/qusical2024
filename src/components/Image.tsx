c11e1ba1b4e593becb191f2ad547f763e6e9c684
yejineee <yygeniee@gmail.com>
2024-12-28 23:40:16 +0900

    chore: @2x 옵션 추가

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -8,7 +8,7 @@ const DAUM_CDN = "//i1.daumcdn.net/thumb/";
 const IMAGE_URL = "https://qusical2025.vercel.app/image/";
 
 function Image({ file, quality = 70, size, ...props }: ImageProps) {
-	const option = encodeURIComponent(`C${size}x${size}.fwebp`);
+	const option = encodeURIComponent(`C${size}x${size}@2x.fwebp`);
 	const filePath = encodeURIComponent(`${IMAGE_URL}${file}`);
 	const src = `${DAUM_CDN}${option}/?fname=${filePath}`;
 	return <img src={src} {...props}></img>;
