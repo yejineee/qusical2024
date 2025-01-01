aabc994d882db77f3b3b5ffc0b747f42bd17bc3a
yejineee <yygeniee@gmail.com>
2025-01-01 17:14:51 +0900

    chore: prod에서 지표 측정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,13 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Analytics } from "@vercel/analytics/react";
-import { isDev } from "~/utils/utils";
+import { isDev, isProd } from "~/utils/utils";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
 			<Outlet />
-			<Analytics />
+			{isProd && <Analytics />}
 			{isDev && <TanStackRouterDevtools />}
 		</>
 	),
