91bb103d1d4ea135655d71db267455e7ece45a8a
yejineee <yygeniee@gmail.com>
2025-01-01 12:41:49 +0900

    feat: 웹 이미지 변경

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,3 +1,5 @@
+import { isDev } from "./utils";
+
 export type ImageUrlType = {
 	file: string;
 	quality?: number;
@@ -6,7 +8,11 @@ export type ImageUrlType = {
 
 const DAUM_CDN = "//i1.daumcdn.net/thumb/";
 const IMAGE_URL = "https://qusical2025.vercel.app/image/";
+const LOCAL_IMAGE_PATH = "/image/";
 export const imageUrl = ({ quality = 70, size, file }: ImageUrlType) => {
+	if (isDev) {
+		return `${LOCAL_IMAGE_PATH}${file}`;
+	}
 	const crop = size ? `C${size}x${size}` : "";
 	const option = encodeURIComponent(`${crop}@2x.fwebp.q${quality}`);
 	const filePath = encodeURIComponent(`${IMAGE_URL}${file}`);
